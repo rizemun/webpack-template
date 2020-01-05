733d5ca2511743f1a323eff48712a326c5ce75fb
rizemun <rizeman01@gmail.com>
2020-01-05 17:32:41 +0300

    :construction: add sass and postcss loaders

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,30 @@ module.exports = {
           test: /\.css/,
           use: [
               MiniCssExtractPlugin.loader,
-              "css-loader"
+              {
+                  loader: 'css-loader',
+                  options: { sourceMap: true }
+              }, {
+                  loader: 'postcss-loader',
+                  options: { sourceMap: true, config: { path:'src/js/postcss.config.js'} }
+              },
+          ]
+      }, {
+          test: /\.scss/,
+          use: [
+              "style-loader",
+              MiniCssExtractPlugin.loader,
+              {
+                  loader: 'css-loader',
+                  options: { sourceMap: true }
+              }, {
+                  loader: 'postcss-loader',
+                  options: { sourceMap: true, config: { path:'src/js/postcss.config.js'} }
+              }, {
+                  loader: 'sass-loader',
+                  options: { sourceMap: true }
+              },
+
           ]
       }]
     },
