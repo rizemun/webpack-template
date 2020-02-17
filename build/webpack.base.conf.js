c678e32a4edb25dd9ebea61a981bb488fca68fa7
Artem Markin <amarkin@oggettoweb.com>
2020-02-17 11:44:13 +0300

    :sparkles: change publicPath

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -20,7 +20,7 @@ module.exports = {
     output: {
         filename: `${PATHS.assets}js/[name].js`,
         path: PATHS.dist,
-        publicPath: '/'
+        publicPath: './'
     },
     module: {
       rules: [{
@@ -87,7 +87,6 @@ module.exports = {
             filename: `${PATHS.assets}css/[name].css`
         }),
         new HtmlWebpackPlugin({
-            hash: false,
             template: `${PATHS.src}/index.html`,
             filename: './index.html'
         }),
